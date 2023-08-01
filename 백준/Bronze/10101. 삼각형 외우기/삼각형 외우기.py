995d52c900c432fd84364bda0bab2cf4b1da68b8
JJong <63280700+entere77@users.noreply.github.com>
2023-08-01 15:21:59 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,9 @@
+angles=[int(input()) for _ in range(3)]
+if angles.count(60)==3:
+    print('Equilateral')
+elif sum(angles)==180 and len(set(angles))==2:
+    print('Isosceles')
+elif sum(angles)==180 and len(set(angles))==3:
+    print('Scalene')
+else:
+    print('Error')
