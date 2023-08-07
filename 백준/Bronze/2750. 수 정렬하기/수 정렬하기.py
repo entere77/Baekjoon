118e829f6d74f994c62a0220c77f79560a67f718
JJong <63280700+entere77@users.noreply.github.com>
2023-08-07 17:59:43 +0900

    [Bronze II] Title: 수 정렬하기, Time: 76 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -3,11 +3,6 @@ nums=[]
 for i in range(n):
     nums.append(int(input()))
 
-for j in range(n):
-    min_index=j
-    for k in range(j+1,n):
-        if nums[min_index]>nums[k]:
-            min_index=k
-    nums[j], nums[min_index] = nums[min_index], nums[j]
-for k in range(n):
-    print(nums[k])
+nums=sorted(nums)
+for i in range(n):
+    print(nums[i])
